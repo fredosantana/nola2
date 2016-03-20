9cf051f3dfe50e456695ec253be21e9f0366de5e
Wil Santana <santana.wil@gmail.com>
2016-03-20 01:15:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nola::Application.config.secret_key_base = 'd1a3e1f4fd9bfe47f9b4a0bb9d2f0feb717babf20c8fd6e25b56787cd0e50e4f6af93d8490f9aa02aee008c8abb01bb5cb52d2a122f7b7cf910e47bbab4109b3'
